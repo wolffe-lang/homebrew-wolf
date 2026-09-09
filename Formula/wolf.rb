a81de21531932d28bb837080fdca3e9f7e2bf6cc
mfwolffe <wolffemf@dukes.jmu.edu>
2026-09-09 19:01:12 -0400

    wolf: v0.2.9 (revision 4c60946)

--- Formula/wolf.rb
+++ Formula/wolf.rb
@@ -10,8 +10,8 @@ class Wolf < Formula
   # a tagged git url, so this build tells the truth.
   url "https://github.com/wolffe-lang/wolf-lang.git",
       using:    :git,
-      tag:      "v0.2.8",
-      revision: "5c729e8779a83611a66f47a63a5276b158ecdec1"
+      tag:      "v0.2.9",
+      revision: "4c60946081e1d1e65dd4b3d98cd69ac86bdf1d38"
   license "GPL-3.0-or-later"
   head "https://github.com/wolffe-lang/wolf-lang.git", branch: "trunk"
 
