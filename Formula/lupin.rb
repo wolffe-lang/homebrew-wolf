bcd6f7a373077e3fbb61473041425c3f2e39cb3d
mfwolffe <wolffemf@dukes.jmu.edu>
2026-09-13 20:44:13 -0400

    lupin 0.1.36 (6e94436)

--- Formula/lupin.rb
+++ Formula/lupin.rb
@@ -8,8 +8,8 @@ class Lupin < Formula
   # `0.1.27+dev.unknown`.
   url "https://github.com/wolffe-lang/wolf-interp.git",
       using:    :git,
-      tag:      "v0.1.34",
-      revision: "b83049a33fe5c44b538ba9ed50de50383ef9b793"
+      tag:      "v0.1.36",
+      revision: "6e94436b2cae285bf2ffc19477600535e33ccbbb"
   license "GPL-3.0-or-later"
   head "https://github.com/wolffe-lang/wolf-interp.git", branch: "trunk"
 
